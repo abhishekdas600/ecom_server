6601aa0e62968394d71b96c3adb354e130c132a7
abhishekdas <dasabhshek522@gmail.com>
2024-06-21 19:33:27 +0530

    disconnected upstash redis

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,7 +1,19 @@
 import Redis from "ioredis"
 
-export const redisClient = new Redis(`${process.env.REDIS_URL}`);
- redisClient.set('foo', 'bar');
+export const redisClient = new Redis({
+    host: process.env.REDIS_HOST, 
+    port: 6379,        
+     password: process.env.REDIS_PASSWORD, 
+});
+
+
+redisClient.on('connect', () => {
+    console.log('Connected to Redis');
+});
+
+redisClient.on('error', (err) => {
+    console.error('Error connecting to Redis:', err);
+});
 
 
 
